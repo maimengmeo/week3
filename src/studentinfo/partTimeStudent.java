c9d4f7f51e3eadf5d6a0c8c480dce6b02e95525d
mai <phamtuye@sheridancollege.ca>
2022-05-26 13:59:24 -0400

    part time student added

---
+++ src/studentinfo/partTimeStudent.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentinfo;
+
+/**
+ *
+ * @author maimengmeo
+ */
+public class partTimeStudent {
+    int numOfCourses;
+    
+    
+}
